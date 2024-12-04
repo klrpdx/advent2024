70f11f3fb1300d7a72b640a404278f993714f8b7
klrpdx <klrpdx@gmail.com>
2024-12-03 17:13:40 -0800

    Day3 part two solution

--- java/src/com/klr/advent/DayThree.java
+++ java/src/com/klr/advent/DayThree.java
@@ -11,22 +11,44 @@ public class DayThree {
 
     private final FileLoader loader;
     private String regex = "mul\\((\\d+),(\\d+)\\)";
+    private String ignoreThis = "don't\\(\\)(.*?)do\\(\\)";
+    private int totalLines = 0;
 
     public DayThree(FileLoader loader) {
         this.loader = loader;
     }
 
+    private String readFile() throws IOException {
+        StringBuilder sb = new StringBuilder();
+        String nextLine;
+        while ((nextLine = loader.readLine()) != null) {
+            nextLine = nextLine.replace("\n", "").replace("\r", "");
+            sb.append(nextLine);
+            totalLines++;
+        }
+        return sb.toString();
+    }
+
+    private String removeDonts(String text) {
+        Pattern pattern = Pattern.compile(ignoreThis);
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            text = matcher.replaceAll("");
+        }
+        return text;
+    }
+
     public int solve() throws IOException {
         int result = 0;
+
+        String text = readFile();
+        text = removeDonts(text);
         Pattern pattern = Pattern.compile(regex);
-        String nextLine = null;
-        while ((nextLine = loader.readLine()) != null) {
-            Matcher matcher = pattern.matcher(nextLine);
-            while (matcher.find()) {
-                int val1 = Integer.parseInt(matcher.group(1));
-                int val2 = Integer.parseInt(matcher.group(2));
-                result = result + (val1 * val2);
-            }
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            int val1 = Integer.parseInt(matcher.group(1));
+            int val2 = Integer.parseInt(matcher.group(2));
+            result = result + (val1 * val2);
         }
         return result;
     }
@@ -35,6 +57,7 @@ public class DayThree {
         FileLoader fileLoader = new FileLoader("/Users/klr/Projects/advent2024/resources/day3input.txt");
         DayThree solver = new DayThree(fileLoader);
         System.out.println("The solution part 1: "+solver.solve());
+        System.out.println("\tLines processed: "+  solver.totalLines);
     }
 
 
--- java/test/com/klr/advent/DayThreeTest.java
+++ java/test/com/klr/advent/DayThreeTest.java
@@ -37,7 +37,21 @@ class DayThreeTest {
         DayThree solver = new DayThree(loader);
         when(loader.readLine()).thenReturn("{!how()'&where()don't()select()@]how()}mul(884,758);-mul(971,475)who()~from()]~mul(358,23)~;").thenReturn(null);
         assertEquals(1139531, solver.solve());
+    }
 
+    @Test
+    void doDontHandling() throws IOException {
+        DayThree solver = new DayThree(loader);
+        when(loader.readLine()).thenReturn("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))\n").thenReturn(null);
+        assertEquals(48, solver.solve());
+    }
+
+
+    @Test
+    void doDontHandlingLineBreaks() throws IOException {
+        DayThree solver = new DayThree(loader);
+        when(loader.readLine()).thenReturn("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64]\n").thenReturn("(mul(11,8)undo()?mul(8,5))\n").thenReturn(null);
+        assertEquals(48, solver.solve());
     }
 
 }
