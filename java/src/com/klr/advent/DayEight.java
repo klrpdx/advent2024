ca86716b568cb8360ed6dbb0bba733e02d181107
klrpdx <klrpdx@gmail.com>
2024-12-08 20:48:15 -0800

    Base classes for Day 8

---
+++ java/src/com/klr/advent/DayEight.java
@@ -0,0 +1,15 @@
+package com.klr.advent;
+
+import com.klr.advent.util.FileLoader;
+
+public class DayEight {
+
+
+    DayEight(FileLoader fileLoader) {}
+
+
+
+    public int solve() {
+        return 0;
+    }
+}
---
+++ java/test/com/klr/advent/DayEightTest.java
@@ -0,0 +1,19 @@
+package com.klr.advent;
+
+import com.klr.advent.util.FileLoader;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class DayEightTest {
+
+
+    @Mock
+    private FileLoader loader;
+
+
+
+}
