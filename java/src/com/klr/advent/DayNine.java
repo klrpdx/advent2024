156ac135d9ce6ec203d1b67b4416226645f8fdc0
klrpdx <klrpdx@gmail.com>
2024-12-10 12:00:15 -0800

    Solution to Day 9, Part I

--- java/src/com/klr/advent/DayNine.java
+++ java/src/com/klr/advent/DayNine.java
@@ -1,5 +1,6 @@
 package com.klr.advent;
 
+import com.klr.advent.util.Defragmenter;
 import com.klr.advent.util.FileLoader;
 
 import java.io.IOException;
@@ -14,9 +15,17 @@ public class DayNine {
 
 
     public long solve() throws IOException {
-        return 0;
+        String input = this.loader.readLine();
+        Defragmenter defragmenter = new Defragmenter(input);
+        return defragmenter.getChecksum(defragmenter.defragment());
     }
 
+
+    public static void main(String[] args) throws IOException {
+        FileLoader fileLoader = new FileLoader("/Users/klr/Projects/advent2024/resources/day9input.txt");
+        DayNine dayNine = new DayNine(fileLoader);
+        System.out.println("Results part I: "+dayNine.solve());
+    }
 
 
 }
