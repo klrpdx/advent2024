c9dcec8644bdd776dcd9c9b54e5d5bac88c3ec1a
klrpdx <klrpdx@gmail.com>
2024-12-10 11:55:08 -0800

    Tests passing

--- java/src/com/klr/advent/util/Defragmenter.java
+++ java/src/com/klr/advent/util/Defragmenter.java
@@ -1,6 +1,8 @@
 package com.klr.advent.util;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Defragmenter {
@@ -8,29 +10,83 @@ public class Defragmenter {
     private final String diskMap;
     private final List<Integer> fileblocks = new ArrayList<Integer>();
     private final List<Integer> freeblocks = new ArrayList<Integer>();
+    private int totalBlocks;
 
     public Defragmenter(String input) {
         diskMap = input;
     }
 
 
-    public List<Integer> getFileBlocks() {
-        return fileblocks;
+    public Integer[] defragment() {
+        processMap();
+        Integer[] fragmented = getArray();
+        return fillFree(fragmented);
     }
 
-    public void defragment() {
+    private Integer[] fillFree(Integer[] fragmented) {
+        Integer[] defragmented = Arrays.copyOf(fragmented, fragmented.length);
+        int end = defragmented.length - 1;
+        for (int i = 0; i < defragmented.length; i++) {
+            if (i > end) {
+                break;
+            }
+            while (defragmented[end] == -1 && end > i) {
+                end--;
+            }
+            if (defragmented[i] == -1) {
+                defragmented[i] = defragmented[end];
+                defragmented[end] = -1;
+            }
+        }
+        return defragmented;
+    }
+
+    private void processMap() {
         char[] chars = diskMap.toCharArray();
         for (int i = 0; i < chars.length; i++) {
-            if (i%2 == 0) {
-                fileblocks.add(Integer.parseInt(String.valueOf(chars[i])));
-            }
-            else {
+            int size = Integer.parseInt(String.valueOf(chars[i]));
+            totalBlocks += size;
+            if (i % 2 == 0) {
+                fileblocks.add(size);
+            } else {
                 freeblocks.add(Integer.parseInt(String.valueOf(chars[i])));
             }
         }
     }
 
+    public long getChecksum(Integer[] array) {
+        long checksum = 0;
+        for (int i=0; i<array.length; i++) {
+            if (array[i] != -1) {
+                checksum += (long) i * array[i];
+            }
+        }
+        return checksum;
+    }
+
+    public List<Integer> getFileBlocks() {
+        return fileblocks;
+    }
+
     public List<Integer> getFreeBlocks() {
         return freeblocks;
+    }
+
+    public Integer[] getArray() {
+        Integer[] array = new Integer[totalBlocks];
+        int index = 0;
+        for (int fb = 0; fb < fileblocks.size(); fb++) {
+            for (int j = 0; j < fileblocks.get(fb); j++) {
+                array[index] = fb;
+                index++;
+            }
+            if (fb < freeblocks.size()) {
+                for (int j = 0; j < freeblocks.get(fb); j++) {
+                    array[index] = -1;
+                    index++;
+                }
+            }
+        }
+        return array;
     }
 }
--- java/test/com/klr/advent/DayNineTest.java
+++ java/test/com/klr/advent/DayNineTest.java
@@ -60,4 +60,65 @@ class DayNineTest {
         assertEquals(expected, fileBlocks);
     }
 
+    @Test
+    void initalArray() {
+        String input = "12345";
+        Defragmenter disk = new Defragmenter(input);
+        disk.defragment();
+        Integer[] expected = new Integer[]{0, -1, -1, 1, 1, 1, -1, -1, -1, -1, 2, 2, 2, 2, 2};
+        assertArrayEquals(expected, disk.getArray());
+    }
+
+    @Test
+    void initalArrayWithZeros() {
+        String input = "12305";
+        Defragmenter disk = new Defragmenter(input);
+        disk.defragment();
+        Integer[] expected = new Integer[]{0, -1, -1, 1, 1, 1, 2, 2, 2, 2, 2};
+        assertArrayEquals(expected, disk.getArray());
+    }
+
+    @Test
+    void longArray() {
+        String input = "2333133121414131402";
+        Defragmenter disk = new Defragmenter(input);
+        disk.defragment();
+        Integer[] expected = new Integer[]{0, 0, -1, -1, -1, 1, 1, 1, -1, -1, -1, 2, -1, -1, -1, 3, 3, 3, -1, 4, 4, -1, 5, 5, 5, 5, -1, 6, 6, 6, 6, -1, 7, 7, 7, -1, 8, 8, 8, 8, 9, 9};
+        assertArrayEquals(expected, disk.getArray());
+    }
+
+    @Test
+    void defragment() {
+        String input = "12345";
+        Defragmenter disk = new Defragmenter(input);
+        Integer[] expected = new Integer[]{0, 2, 2, 1, 1, 1, 2, 2, 2, -1, -1, -1, -1, -1, -1};
+        assertArrayEquals(expected, disk.defragment());
+    }
+
+    @Test
+    void defragmentLong() {
+        String input = "2333133121414131402";
+        Defragmenter disk = new Defragmenter(input);
+        Integer[] expected = new Integer[]{0,0,9,9,8,1,1,1,8,8,8,2,7,7,7,3,3,3,6,4,4,6,5,5,5,5,6,6,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
+        assertArrayEquals(expected, disk.defragment());
+    }
+
+    @Test
+    void getChecksum() {
+        String input = "12345";
+        Defragmenter disk = new Defragmenter(input);
+        Integer[] defragmented =  disk.defragment();
+        int expected = 60;
+        assertEquals(expected, disk.getChecksum(defragmented));
+    }
+
+    @Test
+    void getChecksumLong() {
+        String input = "2333133121414131402";
+        Defragmenter disk = new Defragmenter(input);
+        Integer[] defragmented =  disk.defragment();
+        int expected = 1928;
+        assertEquals(expected, disk.getChecksum(defragmented));
+    }
+
 }
