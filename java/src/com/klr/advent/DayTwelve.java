063b0ecc3b76afe6d5f7f2139571dc8f6c64e0c3
klrpdx <klrpdx@gmail.com>
2024-12-12 20:24:57 -0800

    Solution day 12, part I

--- java/src/com/klr/advent/DayTwelve.java
+++ java/src/com/klr/advent/DayTwelve.java
@@ -1,8 +1,39 @@
 package com.klr.advent;
 
+import com.klr.advent.util.FileLoader;
+import com.klr.advent.util.Garden;
+
+import java.io.IOException;
+
 public class DayTwelve {
 
+    private final FileLoader fileLoader;
+    private String asciiMap;
 
+    public DayTwelve(FileLoader fileLoader) {
+        this.fileLoader = fileLoader;
+    }
+
+    private void loadAsciiMap() throws IOException {
+        StringBuilder builder = new StringBuilder();
+        String nextLine = null;
+        while ((nextLine = fileLoader.readLine()) != null) {
+            builder.append(nextLine).append("\n");
+        }
+        asciiMap = builder.toString();
+    }
+
+    public long solvePart1() throws IOException {
+        loadAsciiMap();
+        Garden garden = new Garden(asciiMap);
+        return garden.getFencePrice();
+    }
+
+    public static void main(String[] args) throws IOException {
+        FileLoader fileLoader = new FileLoader("/Users/klr/Projects/advent2024/resources/day12input.txt");
+        DayTwelve dayTwelve = new DayTwelve(fileLoader);
+        System.out.println("Part I solution: "+dayTwelve.solvePart1());
+    }
 
 
 
--- java/src/com/klr/advent/util/Plot.java
+++ java/src/com/klr/advent/util/Plot.java
@@ -30,7 +30,7 @@ public class Plot {
         long perimeter = 0;
 
         for (Vertex v : getVertices()) {
-            perimeter += 4 - v.getSiblingNeighbors();
+            perimeter += 4L - v.getSiblingNeighbors();
         }
         return perimeter;
     }
